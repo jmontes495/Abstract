942c6a4e09887aca347271c70a5741cf2a201fc7
Juliana Montes <j.montes495@uniandes.edu.co>
2018-05-12 14:54:02 -0500

    Creación: Figura se mueve y rota

---
+++ Abstracto/Assets/RotarAbstracto.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotarAbstracto : MonoBehaviour {
+
+    float speed = 5f;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+    void FixedUpdate()
+    {
+        float moveHorizontal = 0f;
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            moveHorizontal = 1f;
+        }
+        else if (Input.GetKey(KeyCode.L))
+        {
+            moveHorizontal = -1f;
+        }
+
+        float moveVertical = 0f;
+        if (Input.GetKey(KeyCode.I))
+        {
+            moveVertical = 1f;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            moveVertical = -1f;
+        }
+        transform.Rotate(moveHorizontal, moveVertical, 0f);
+    }
+}
---
+++ Abstracto/Assets/scripts/MoverJugador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoverJugador : MonoBehaviour
+{
+    float speed = 5f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float adelante = 0f;
+        if(Input.GetKey(KeyCode.Space))
+        {
+            adelante = 1f;
+        }
+        else if(Input.GetKey(KeyCode.LeftShift))
+        {
+            adelante = -1f;
+        }
+
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal, moveVertical,adelante);
+        rb.velocity = movement * speed; 
+    }
+}
